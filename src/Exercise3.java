cb9c9a74cfcab01b256d8655e0c665f9e8dbab83
Patrycja Karczewska <karczewska.patrycja@wp.pl>
2021-10-23 13:36:13 +0200

    new commit

---
+++ src/Exercise3.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    //    Napisz program służący do rozwiązywania równania kwadratowego. Program powinien pobrać trzy liczby całkowite
+//    (współczynniki równania kwadratowego a, b, c) i wyliczyć pierwiastki x1, x2 równania
+//    Jeśli delta ∆ wyjdzie ujemna, wypisz „Delta ujemna” i zakończ program.
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj współczynniki równania kwadratowego:");
+        System.out.println("Podaj a:");
+        int a = scanner.nextInt();
+
+        System.out.println("Podaj b:");
+        int b = scanner.nextInt();
+
+        System.out.println("Podaj c:");
+        int c = scanner.nextInt();
+
+        scanner.close();
+
+        quadraticEquation(a, b, c);
+
+    }
+
+    static void quadraticEquation(int a, int b, int c) {
+
+        int delta = (int) Math.pow(b, 2) - 4 * a * c;
+        if (delta >= 0) {
+
+            System.out.println(delta);
+
+            float x1 = (-(float) b - (float) Math.sqrt(delta)) / (2 * a);
+            System.out.println("x1=" + x1);
+
+
+            float x2 = (-(float) b + (float) Math.sqrt(delta)) / (2 * a);
+            System.out.println("x2=" + x2);
+        } else {
+            System.out.println("Delta jest ujemna, ne da się wyliczyć pierwiastków równania");
+
+        }
+
+
+    }
+}
