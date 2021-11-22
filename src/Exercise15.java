03e72b4e48e8dd9787d985fb31775d78dc7205d7
Patrycja Karczewska <karczewska.patrycja@wp.pl>
2021-11-22 17:20:18 +0100

    new commit

--- src/Exercise15.java
+++ src/Exercise15.java
@@ -5,8 +5,6 @@ public class Exercise15 {
     public static void main(String[] args) {
 
 
-
-
         //Napisz program, który pobierze od użytkownika 10 dowolnie dużych liczb (zmiennych typu int) i wypisze te, które wystąpiły minimum dwukrotnie
 
         Scanner scanner = new Scanner(System.in);
@@ -15,41 +13,42 @@ public class Exercise15 {
         int userInt2 = scanner.nextInt();
         int userInt3 = scanner.nextInt();
         int userInt4 = scanner.nextInt();
-//        int userInt5 = scanner.nextInt();
-//        int userInt6 = scanner.nextInt();
-//        int userInt7 = scanner.nextInt();
-//        int userInt8 = scanner.nextInt();
-//        int userInt9 = scanner.nextInt();
-//        int userInt10 = scanner.nextInt();
-
-//        String userInt11 = scanner.nextLine();
-//        String userInt12 = scanner.nextLine();
-//
-//
-//        boolean match = userInt12.matches(userInt11);
+        int userInt5 = scanner.nextInt();
+        int userInt6 = scanner.nextInt();
+        int userInt7 = scanner.nextInt();
+        int userInt8 = scanner.nextInt();
+        int userInt9 = scanner.nextInt();
+        int userInt10 = scanner.nextInt();
 
 
-        int[] arrayUserInt = new int[4];
+        int[] arrayUserInt = new int[10];
         arrayUserInt[0] = userInt1;
         arrayUserInt[1] = userInt2;
         arrayUserInt[2] = userInt3;
         arrayUserInt[3] = userInt4;
+        arrayUserInt[4] = userInt5;
+        arrayUserInt[5] = userInt6;
+        arrayUserInt[6] = userInt7;
+        arrayUserInt[7] = userInt8;
+        arrayUserInt[8] = userInt9;
+        arrayUserInt[9] = userInt10;
 
-        int array=0;
+
+        int array = 0;
 
 
         for (int i = 0; i < arrayUserInt.length; i++) {
-            for (int j=0 ; j < arrayUserInt.length; j++) {
-                if (i!=j && arrayUserInt[i] == arrayUserInt[j]) {
+            for (int j = i+1; j < arrayUserInt.length; j++) {
+                if (i != j && arrayUserInt[i] == arrayUserInt[j]) {
 
-                     array =  arrayUserInt[j];
-
+                    array = arrayUserInt[i];
+                    System.out.println(array);
+                    break;
+                }
             }
 
-            }System.out.println(array);
+
         }
-
-
     }
 }
 
